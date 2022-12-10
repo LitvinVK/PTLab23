887ed92241543191455d624c3033bb4242d718ef
LitvinVK <LitvinVK@yandex.ru>
2022-12-10 17:24:53 +0300

    individual task ready

--- shop/views.py
+++ shop/views.py
@@ -15,7 +15,14 @@ class PurchaseCreate(CreateView):
     model = Purchase
     fields = ['product', 'person', 'address']
 
+    def update_count(self):
+        Product.objects.filter(id=self.object.product.id).update(count=self.object.product.count-1)
+
     def form_valid(self, form):
         self.object = form.save()
-        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
-
+        prods = Product.objects.get(id=self.object.product.id)
+        if (prods.count) <= 0:
+            return HttpResponse(f'Ошибка: товаров больше не осталось! <a href="/">Главная</a>')
+        else:
+            PurchaseCreate.update_count(self)
+            return HttpResponse(f'Спасибо за покупку! <a href="/">Главная</a>')
